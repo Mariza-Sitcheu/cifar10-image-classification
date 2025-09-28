29e479b76640a66396a5d13407391bb5a53cabb0
Yamdjeu Sitcheu, Mariza <marizasitcheu@yahoo.fr>
2025-09-28 15:48:21 +0200

    custom model and resnet18 pretrained model defined

---
+++ src/model.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class CustomCNN(nn.Module):
+    def __init__(self):
+        super(CustomCNN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc1 = nn.Linear(64 * 8 * 8, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.25)
+
+    def forward(self, x):
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = x.view(-1, 64 * 8 * 8)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
+
+
+def get_resnet18(pretrained=True):
+    model = models.resnet18(pretrained=pretrained)
+    model.fc = nn.Linear(model.fc.in_features, 10)  # Modify for 10 classes
+    return model
